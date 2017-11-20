7a4cf8e84337836f6cd57fc81db56cb24e0ef810
Renan Godoi <renangodoimedeiros@gmail.com>
2017-11-20 21:33:22 -0200

    prototipo html do projeto de SI

---
+++ js/include.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+	var head = document.getElementById("header");
+	if (head!= null){
+		document.getElementById("header").innerHTML = '\
+		\
+		<meta charset="utf-8">\
+		<title>Prototipo</title>\
+		<link rel="stylesheet" type="text/css" href="css/estilo.css"/>\
+		<link rel="stylesheet" href="css/circle.css">\
+		\
+		';
+	}
+
+	document.getElementById("pagina").innerHTML += '\
+	\
+	<h1 style="color:white;">Protótipo Transparência Política</h1>\
+    	<nav>\
+			<ul class="menu">\
+				<li id="active"><a href="./principal.html" id="current">Home</a></li>\
+				<li><a href="#">Metas</a>\
+				<ul>\
+					<li><a href="./Metas_governador.html">Metas do Governador</a></li>\
+					<li><a href="#">Metas do Deputado Federal</a></li>\
+					<li><a href="#">Metas do Senador</a></li>\
+				</ul>\
+				</li>\
+				<li><a href="./Impostometro.html">Impostômetro</a></li>\
+				<li><a href="./Politica.html">Política Brasília</a></li>\
+			</ul>\
+		</nav>\
+		</br></br></br>\
+		<p style="color:white;">Nome do Partido</br>Dados do governador Atual</p>\
+	\
+	';
+}
