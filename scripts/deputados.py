1b3f605f816ec41f52e59e9372ca67d8f2cdf2c1
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-11-27 14:27:09 -0200

    Script para buscar dados dos deputados.

---
+++ scripts/deputados.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import requests
+import json
+
+dados = {}
+next_url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?ordenarPor=nome'
+
+while next_url:
+    print("Baixando dados dos deputados em {}".format(next_url))
+    resp = requests.get(next_url)
+
+    if resp.status_code != 200:
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+
+    # Adiciona as novas informacoes no dicionario
+    for deputado in resp.json()['dados']:
+        dados[deputado['nome']] = deputado
+
+    # Encontra a proxima url se tiver
+    next_url = ''
+    for link in resp.json()['links']:
+        if link['rel'] == 'next':
+            next_url = link['href']
+            break
+
+
+# Escreve os dados em um arquivo
+with open('../dados/deputados.json', 'w') as fp:
+    json.dump(dados, fp, sort_keys=True, indent=4)
